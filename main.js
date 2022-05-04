aa248f2ffb985e16a52b0696303b02bb11820dbb
unknown <vizuetadaniel85@gmail.com>
2022-05-04 15:46:42 -0500

    CalcDone

--- main.js
+++ main.js
@@ -31,14 +31,24 @@ const subtract = (numA, numB) => {
 
 const multiply = (numA, numB) => {
   // * to get a product then return it
+  const product = numA * numB
+  return product
   // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
   console.log(numA, numB)
 }
 
-const divide = null
+const divide = (numberA, numB) => {
+  const keys = numberA / numB
+  return keys
+}
 // / to get a quotient,
 
-const modulus = null
+const modulus = (numA, numB) => {
+  const remainder = numA % numB
+  return remainder
+}
+
+
 // and % to get a remainder.
 
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
@@ -64,11 +74,11 @@ const equals = () => {
     break;
     case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
     break;
-    case "multiplication": multiply(firstNum, secondNum) 
+    case "multiplication": putResultInElement(multiply(firstNum, secondNum)) 
     break;
-    case "division": console.log(divide(firstNum, secondNum)) 
+    case "division":  putResultInElement(divide(firstNum, secondNum)) 
     break;
-    case "modulus": console.log(modulus(firstNum, secondNum)) 
+    case "modulus": putResultInElement(modulus(firstNum, secondNum)) 
     break;
     default: "Choose an operation"
   }
